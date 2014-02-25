5bc179a4a1b57531513f01d820d8096af4844109
danhnguyen <danhnguyen0902@gmail.com>
2014-02-25 15:30:59 -0500

    Facebook API

---
+++ APIApp/gen/com/program/firstapiapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.program.firstapiapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ DanhNguyenApp/gen/com/program/danhnguyenapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.program.danhnguyenapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
